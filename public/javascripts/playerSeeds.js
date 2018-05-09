d7a65a370338f2f397c7b1698b55d479e6cfb907
Diego Leal <diegoleallinux@gmail.com>
2018-05-09 10:51:34 -0400

    module fix

--- public/javascripts/playerSeeds.js
+++ public/javascripts/playerSeeds.js
@@ -1,5 +1,5 @@
-const MySportsFeeds = require("mysportsfeeds-node");
-var msf = new MySportsFeeds("1.2", true);
-msf.authenticate("diegolealb", "3478561a");
+// const MySportsFeeds = require("mysportsfeeds-node");
+// var msf = new MySportsFeeds("1.2", true);
+// msf.authenticate("diegolealb", "3478561a");
 
-var data = msf.getData('nfl', '2016-2017-regular', 'cumulative_player_stats', 'json', {});
+// var data = msf.getData('nfl', '2016-2017-regular', 'cumulative_player_stats', 'json', {});
